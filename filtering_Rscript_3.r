20097eb6466b153e5c36a83059d20ede73994c52
Aleksandra Kornienko <79104699+aleksandrakornienko@users.noreply.github.com>
2023-03-15 19:08:04 +0100

    Delete filtering_Rscript_3.r

--- filtering_Rscript_3.r
+++
@@ -1,9 +0,0 @@
-
-bidirTSS=Sys.getenv("bidirTSS_loc") 
-transcripts_5=Sys.getenv("transcripts_loc") 
-transcripts_bidir=Sys.getenv("transcripts_bidir_loc")
-a<-read.table(bidirTSS, header=F)
-b<-read.table(transcripts_5, header=F)
-c<-b[b$V4 %in% a$V4,]
-write.table(c, file=transcripts_bidir, col.names=F, row.names=F , quote=F, sep="\t")
-
