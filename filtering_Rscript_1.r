b6d05501a3b59bfd096885e74490244e0fc818f8
Aleksandra Kornienko <79104699+aleksandrakornienko@users.noreply.github.com>
2023-03-15 19:07:43 +0100

    Delete filtering_Rscript_1.r

--- filtering_Rscript_1.r
+++
@@ -1,8 +0,0 @@
-
-genes_strand=Sys.getenv("genes_strand_location") 
-genes=Sys.getenv("genes_location") 
-a<-read.table(genes_strand, header=F)
-b<-read.table(genes, header=F)
-c<-merge(a,b, by="V1")
-write.table(c, file=genes, col.names=F, row.names=F , quote=F, sep="\t")
-
